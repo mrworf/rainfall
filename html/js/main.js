0c52099733058fe486e930f689fc34360e613210
Henric Andersson <henric@sensenet.nu>
2020-05-16 16:15:55 -0700

    Shows all sprinklers
    Nothing more

--- html/js/main.js
+++ html/js/main.js
@@ -1,24 +1,25 @@
-function generateSprinker(id, name)
+function generateSprinkler(sprinkler)
 {
-  html  = '<div style="display: none" id="sprinkler' + id + '">';
-  html += '<button id="override' + id + '">Manual</button>';
-  html += '<button id="toggle' + id + '">Enable</button>';
-  html += '<span class="name">' + name + '</span>';
+  html  = '<div id="sprinkler' + sprinkler.id + '">';
+  html += '<button id="delete' + sprinkler.id + '">Delete</button>';
+  html += '<button id="override' + sprinkler.id + '">Manual</button>';
+  html += '<button id="toggle' + sprinkler.id + '">Enable</button>';
+  html += '<span class="name">' + sprinkler.name + '</span>';
   html += 'Runtime';
-  html += '<select id="runtime' + id + '">';
+  html += '<select id="runtime' + sprinkler.id + '">';
   html += '<option value="1">1 minute</option>';
   html += '<option value="5">5 minutes</option>';
   html += '<option value="10">10 minutes</option>';
   html += '<option value="15">15 minutes</option>';
   html += '</select>';
   html += 'Cycles';
-  html += '<select id="cycles' + id + '">';
+  html += '<select id="cycles' + sprinkler.id + '">';
   html += '<option value="1">Once</option>';
   html += '<option value="2">Twice</option>';
   html += '<option value="3">Trice</option>';
   html += '</select>';
   html += 'Every';
-  html += '<select id="stepping' + id + '">';
+  html += '<select id="stepping' + sprinkler.id + '">';
   html += '<option value="1">day</option>';
   html += '<option value="2">other day</option>';
   html += '<option value="3">3rd day</option>';
@@ -41,12 +42,15 @@ function generateSprinker(id, name)
 function setup() {
   html = "";
   $.ajax({
-    url:"/sprinkers",
+    url:"/sprinklers",
     type:"GET",
   }).done(function(e, data){
     for (s in e) {
+      s = e[s];
       console.log(s);
+      html += generateSprinkler(s);
     }
+    $('#list').html(html);
   }).fail(function(e, data) {
     alert('Unable to load sprinklers')
   });
